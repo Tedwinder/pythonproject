76407d5d144a1cf2e08ba5701e465f81d6e5f5b3
Tedwinder <TedCallow76@gmail.com>
2016-03-09 04:23:43 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,16 @@
+from flask import Flask
+import os
 
-//from flask import Flask
-//import os
-
-//app = Flask(__name__)
+app = Flask(__name__)
 
 
-//@app.route('/')
+@app.route('/')
+def index():
+    return "Hello, World!"
 
-def printTable(tableData):
-    colWidths = [0]*len(tableData)
-    for col in range(len(tableData)):
-        for row in range(len(tableData[0])):
-            if len(tableData[col][row]) > colWidths[col]:
-                colWidths[col] = len(tableData[col][row])
-    for row in range(len(tableData[0])):
-        for col in range(len(tableData)):
-            print(tableData[col][row])
-        print("")
 
-#tableData = [['<h3>oranges</h3>', '<h3>bananas</h3>', '<h3>cherries</h3>', '<h3>potato</h3>'], ['<h3>oranges</h3>', '<h3>bananas</h3>', '<h3>cherries</h3>', '<h3>potato</h3>']]
-tableData = [['apples', 'oranges', 'cherries', 'banana'], ['Alice', 'Bob', 'Carol', 'David']]
-printTable(tableData)
+if __name__ == '__main__':
+    # gets Heroku's suggested port out of the environment dictionary if exists:
+    port = int(os.environ.get('PORT', 5000))
+    # this is the wsgi hook:
+    app.run(host='0.0.0.0', port=port)
