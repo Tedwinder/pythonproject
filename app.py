c62e4e77e1badee3fa2ebe5fa60957405c229469
Tedwinder <TedCallow76@gmail.com>
2016-03-09 20:11:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def printTable(tableData):
                 colWidths[col] = len(tableData[col][row])
     for row in range(len(tableData[0])):
         for col in range(len(tableData)):
-            print(tableData[col][row].rjust(colWidths[col]),end=" ")
+            print(tableData[col][row].rjust(colWidths[col]))
         print("")
         
 tablePrinter = [1, 2, 3,4]
